a6d40a2bcf31455a3be7433279a8a668812e8b23
purealexiafast <106492503+purealexiafast@users.noreply.github.com>
2023-03-09 17:40:55 -0800

    handlebar connection code

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const sequelize = require('./config/connection');
+const path = require("path")
+const exphbs = require("express-handlebars")
+
 
 const Car = require('./models/Car');
 const Customer = require('./models/Customer');
@@ -11,3 +14,40 @@ const PORT = process.env.PORT || 3001;
 sequelize.sync().then(() => {
   app.listen(PORT, () => console.log('Now listening'));
 });
+
+
+//----HandleBar Set Up--
+
+
+const hbs = exphbs.create() //if we have helpers, add them within create()
+
+
+app.engine("handlebars", hbs.engine)
+app.set("view engine", "handlebars")
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+//routes must be in controllers folder --
+
+app.get("/", (req, res) => {
+  res.render("main")
+})
+
+app.get("/gallery", (req, res) => {
+  res.render("gallery")
+})
+
+app.get("/home", (req, res) => {
+  res.render("home")
+})
+
+app.get("/rent", (req, res) => {
+  res.render("rent")
+})
+
+app.listen(3001, () => {
+  console.log("app listening")
+})
