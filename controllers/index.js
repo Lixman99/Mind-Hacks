1e041a1d90a1b7c178637d3694a8376a6b163b42
Bunix25 <svattt7@gmail.com>
2023-03-10 15:46:39 -0500

    commit

--- controllers/index.js
+++ controllers/index.js
@@ -1,9 +1,17 @@
 const router = require('express').Router();
 
+const reservationRoutes = require('./reservation-routes');
 const apiRoutes = require('./api');
-const homeRoutes = require('./home-routes.js');
+const homeRoutes = require('./home-routes');
+const userRoutes = require('./api/user-routes');
+
+
+
 
 router.use('/', homeRoutes);
 router.use('/api', apiRoutes);
+router.use('/',reservationRoutes );
+router.use('/api', userRoutes);
+
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const sequelize = require('./config/connection');
 
+
 const path = require("path")
-const express = require("express")
 const exphbs = require("express-handlebars")
 const hbs = exphbs.create()
+const app = express()
 
 app.engine("handlebars", hbs.engine)
 app.set("view engine", "handlebars")
@@ -13,25 +14,25 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get("/home", (req, res) => {
-    res.render("homepage")
-})
+// app.get("/home", (req, res) => {
+//     res.render("homepage")
+// })
 
-app.get("/galery", (req, res) => {
+// app.get("/gallery", (req, res) => {
 
-})
+// })
 
-app.get("/rent", (req, res) => {
-    res.render("contact")
-})
+// app.get("/rent", (req, res) => {
+//     res.render("contact")
+// })
 
 
 const Car = require('./models/Car');
 const Customer = require('./models/Customer');
 const Reservation = require('./models/Reservation');
 
-
-const app = express();
+app.use(require("./controllers"));
+;
 const PORT = process.env.PORT || 3001;
 // Connect to the database before starting the Express.js server
 sequelize.sync().then(() => {
