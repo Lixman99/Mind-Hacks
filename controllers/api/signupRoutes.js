0aa1b8d1e895660fa5998a7d7d698ee558ca674d
Lixman <lixman_d@outlook.com>
2023-03-13 20:29:47 -0400

    Added the handlebars for the login and log out.+ Starting the front and back and js of the sign up.

---
+++ controllers/api/signupRoutes.js
@@ -0,0 +1,24 @@
+const router = require("express").Router();
+const { Customer } = require(`../../models`);
+
+router.post("/signup", async (req, res) => {
+  try {
+    const customer = await Customer.create(req.body);
+
+    // if (customerData) {
+    //   res.status(400).json({ message: "Please use an other name" });
+    //   return;
+    // }
+
+    req.session.save(() => {
+      req.session.user_id = customer.id;
+      req.session.logged_in = true;
+        // req.session.isAdmin = "someEmail@email.com"===customer.email
+      res.json({ customer, message: "Account created" });
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
