2e054a2cf17b4a7b03bb4f78656a4cf54590f18d
purealexiafast <106492503+purealexiafast@users.noreply.github.com>
2023-03-13 17:57:16 -0700

    added home page handlebar and css and jasvascript, added gallery page handlebar and css

--- server.js
+++ server.js
@@ -13,36 +13,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-
-
-
-const PORT = process.env.PORT || 3001;
-// Connect to the database before starting the Express.js server
-
-app.get("/", (req, res) => {
-  res.render("main")
-})
-
-app.get("/gallery", (req, res) => {
-  res.render("gallery")
-})
-
-// app.get("/home", (req, res) => {
-//     res.render("homepage")
-// })
-
-
-// app.get("/gallery", (req, res) => {
-
-// })
-
-// app.get("/rent", (req, res) => {
-//     res.render("contact")
-// })
-
-
-
-
 app.use(require("./controllers"));
 
 
