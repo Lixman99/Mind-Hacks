184df733824702c037bbf8f6af1213196b38eb79
Lixman <lixman_d@outlook.com>
2023-03-16 18:47:40 -0400

    quick save befor pulling from dev

--- public/js/singup.js
+++ public/js/singup.js
@@ -1,31 +1,38 @@
-const signupFormHandler = async (event) =>{
-    event.preventDefault();
+const signupFormHandler = async (event) => {
+  event.preventDefault();
 
-    const name = document.querySelector(`#name-signup`).value.trim();
-    const email = document.querySelector(`#email-signup`).value.trim();
-    const password =document.querySelector(`#password-signup`).value.trim();
-    const phone =document.querySelector(`#phone-signup`).value.trim();
+  const name = document.querySelector(`#name-signup`).value.trim();
+  const email = document.querySelector(`#email-signup`).value.trim();
+  const password = document.querySelector(`#password-signup`).value.trim();
+  const phone = document.querySelector(`#phone-signup`).value.trim();
 
-    if (email && password) {
-        const response = await fetch(`/api/customer/login`,{
-            method: `POST`,
-            body:JSON.stringify({email,password, phone,name}),
-            headers: { "Content-Type":"application/json" }
-        });
+  if (email) {
+    const response = await fetch(`/api/customer/signup`, {
+      method: `POST`,
+      body: JSON.stringify({ email }),
+      headers: { "Content-Type": "application/json" },
+    });
 
-        if (response.ok){
-            document.location.replace(`/`)
-        } else {
-            alert(`Sign up failed, try agin`)
-        }
+    if (email.ok) {
+      alert(`Use a diferent email`);
     }
+  }
+
+  if (email && password) {
+    const response = await fetch(`/api/customer/login`, {
+      method: `POST`,
+      body: JSON.stringify({ email, password, phone, name }),
+      headers: { "Content-Type": "application/json" },
+    });
+
+    if (response.ok) {
+      document.location.replace(`/`);
+    } else {
+      alert(`Sign up failed, try agin`);
+    }
+  }
 };
-// const req.body = {
-//     email: "someemeial@email.com",
-//     password: "abc123",
-//     name: "bob",
-//     phone: 123456789
-// }
+
 document
-    .querySelector(`.signup-form`)
-    .addEventListener(`submit`, loginFormHandler);
+  .querySelector(`.signup-form`)
+  .addEventListener(`submit`, loginFormHandler);
