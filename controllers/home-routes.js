b7039f92418b6573de72ddd25ea6bacb13610acd
Teri Phillips <phillips.terianne@gmail.com>
2023-03-19 23:36:25 -0400

    picture changes

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -135,7 +135,7 @@ router.get(`/`, async (req, res) => {
 
 router.get(`/login`, (req, res) => {
   if (req.session.logged_in) {
-     res.redirect(`/`);
+     res.redirect(`/home`);
      return;
   }
   res.render(`login`);
--- models/Reservation.js
+++ models/Reservation.js
@@ -3,57 +3,6 @@ const sequelize = require('../config/connection');
 const nodemailer = require("nodemailer");
 const { EMAIL, PASSWORD } = require('../env');
 
-const emailTemplate = `
-<!DOCTYPE html>
-<html>
-  <head>
-    <style>
-      .container {
-        font-family: Arial, sans-serif;
-        max-width: 600px;
-        margin: auto;
-        background-color: #f5f5f5;
-        padding: 30px;
-      }
-      .header {
-        font-size: 24px;
-        color: #333;
-      }
-      .content {
-        font-size: 16px;
-        color: #333;
-      }
-      .list {
-        font-size: 14px;
-        list-style-type: none;
-      }
-      .list-item {
-        margin-bottom: 8px;
-      }
-    </style>
-  </head>
-  <body>
-    <div class="container">
-      <h1 class="header">Reservation Confirmation</h1>
-      <p class="content">
-        Thank you for reserving a car with us. Your reservation details are as
-        follows:
-      </p>
-      <ul class="list">
-        <li class="list-item">Car: ${this.carTitle}</li>
-        <li class="list-item">Pickup Date: ${this.pickup}</li>
-        <li class="list-item">Return Date: ${this.return}</li>
-        <li class="list-item">Price: ${this.price}</li>
-      </ul>
-      <p class = "content">Pickup time is 2PM and return time is 11AM</p>
-      <p class="content">
-        Thank you for choosing our car rental service!
-      </p>
-    </div>
-  </body>
-</html>
-`;
-
 class Reservation extends Model {
   async sendConfirmationEmail(customerEmail, carTitle) {
     // Define email transport settings
@@ -61,11 +10,11 @@ class Reservation extends Model {
       host: "smtp.gmail.com", // SMTP server address (usually mail.your-domain.com)
       port: 465, // Port for SMTP (usually 465)
       secure: true, // Usually true if connecting to port 465
-      auth: {
+       auth: {
         user: EMAIL, // Your email address
         pass: PASSWORD, // Password (for gmail, your app password)
         // ⚠️ For better security, use environment variables set on the server for these values when deploying
-      },
+       },
     });
 
     // Define email message settings
