ca6506849aa7230a378a23499c8da544e8bdbd46
Teri Phillips <phillips.terianne@gmail.com>
2023-03-21 07:43:11 -0400

    heroku fixes

--- models/index.js
+++ models/index.js
@@ -1,14 +1,16 @@
 const Car = require('./Car');
 const Reservation = require('./Reservation');
 const Customer = require("./Customer")
-
-Car.hasMany(Reservation, {
-  foreignKey: 'car_id',
-});
-
-Reservation.belongsTo(Car, {
-  foreignKey: 'car_id',
-});
+Car.belongsToMany(Customer,{
+  through: Reservation,
+  as: "car_reservation",
+  foreignKey: "carId"
+})
+Customer.belongsToMany(Car,{
+  through: Reservation,
+  as: "customer_reservation",
+  foreignKey: "customerId"
+})
 
 module.exports = { Car, Reservation, Customer };
 
